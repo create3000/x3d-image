8377155ff874815609fd0ce1d05ad73943c17439
Holger Seelig <holger.seelig@gmail.com>
2024-03-01 12:42:01 +0100

    Removed Makefile.

--- Makefile
+++
@@ -1,4 +0,0 @@
-.PHONY: tests
-tests:
-	npx . -d 1 -s 1600x900 -i tests/IndexedFaceSet/IndexedFaceSet.x3d -o tests/test.jpg
-	npx . -a -s 1600x900 -i tests/IndexedFaceSet/IndexedFaceSet.x3d -o tests/view-all.png
---
+++ build/test.js
@@ -0,0 +1,7 @@
+#!/usr/bin/env node
+"use strict";
+
+const { systemSync } = require ("shell-tools");
+
+systemSync ("npx . -d 1 -s 1600x900 -i tests/IndexedFaceSet/IndexedFaceSet.x3d -o tests/test.jpg");
+systemSync ("npx . -a -s 1600x900 -i tests/IndexedFaceSet/IndexedFaceSet.x3d -o tests/view-all.png");

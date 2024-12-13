c07365cf1eaf88e2c5b6f23d039e59c54c75e738
Holger Seelig <holger.seelig@gmail.com>
2024-12-13 16:29:56 +0100

    Added test.

--- build/test.js
+++ build/test.js
@@ -7,4 +7,5 @@ systemSync (`npx . -d 1 -s 1600x900 -i tests/IndexedFaceSet/IndexedFaceSet.x3d -
 systemSync (`npx . -a -s 1600x900 -i tests/IndexedFaceSet/IndexedFaceSet.x3d -o tests/view-all.png`);
 systemSync (`npx . -s 1600x900 -i "${__dirname}/../tests/IndexedFaceSet/IndexedFaceSet.x3d" -o "${__dirname}/../tests/abs.png"`);
 systemSync (`npx . -d 1 -s 1600x900 -i https://create3000.github.io/media/examples/Geometry3D/IndexedFaceSet/IndexedFaceSet.x3d -o tests/https.jpg`);
-systemSync (`npx . -s 1600x900 tests/IndexedFaceSet/IndexedFaceSet.x3d tests/test.no.jpg`);
+systemSync (`npx . tests/IndexedFaceSet/IndexedFaceSet.x3d tests/test.no.jpg`);
+systemSync (` npx . -a -e https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Assets/master/Models/ClearCoatCarPaint/glTF/ClearCoatCarPaint.gltf tests/env-light.png`);

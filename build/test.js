ed6340dead9f724234c62ac14d8fbd20b130fc91
Holger Seelig <holger.seelig@gmail.com>
2025-09-11 10:09:23 +0200

    Use -l for logging.

--- build/test.js
+++ build/test.js
@@ -12,4 +12,4 @@ systemSync (`npx . -d 1 -s 1600x900 -i https://create3000.github.io/media/exampl
 systemSync (`npx . tests/IndexedFaceSet/IndexedFaceSet.x3d tests/test.no.jpg`);
 systemSync (` npx . -a -w HELIPAD -i https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Assets/master/Models/ClearCoatCarPaint/glTF/ClearCoatCarPaint.gltf -o tests/env-light.png`);
 systemSync (`npx . -a -w CANNON -b white -r "0.509086470924499 -0.842639596032569 -0.175469303069548 0.42682311202636" -c LINEAR -x 1.5 -m KHR_PBR_NEUTRAL -i https://raw.githubusercontent.com/KhronosGroup/glTF-Sample-Assets/master/Models/BoomBox/glTF/BoomBox.gltf -o tests/boombox.png`);
-systemSync (`npx . -l -n -i https://create3000.github.io/media/examples/Rendering/OrderIndependentTransparency/OrderIndependentTransparency.x3d -o tests/oit.png`);
+systemSync (`npx . -n -g -i https://create3000.github.io/media/examples/Rendering/OrderIndependentTransparency/OrderIndependentTransparency.x3d -o tests/oit.png`);
